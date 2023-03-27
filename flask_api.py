99fda3c7fd51f9b60b1eceaea69fe7a6c929db87
coderFeedForwardAlg <gaspersscottma@gmail.com>
2023-03-26 21:37:09 -0400

    added_food

--- flask_api.py
+++ flask_api.py
@@ -9,7 +9,12 @@ class testApi(Resource):
      def get(self, name):
          return {"v3, name is: ": name}
 
+class testApi2(Resource):
+     def get(self, food):
+         return {"v3, favoret food is: ": food}
+
 api.add_resource(testApi, "/test/<string:name>")
+api.add_resource(testApi2, "/test2/<string:name>")
 
 if __name__ == '__main__':
     from waitress import serve
